2824729b4004b97b4b2a2254596cce596655f972
jrnold <jeffrey.arnold@gmail.com>
2015-12-13 20:10:56 -0800

    misc

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dictionary of American Naval Fighting Ships (DANFS)
+
+Code to download data from the [Dictionary of American Naval Fighting Ships](http://www.history.navy.mil/research/histories/ship-histories/danfs.html) to a sqlite database.
+
+Running
+```shell
+$ python danfs.py
+```
+creates a sqlite3 database named `danfs.sqlite3` with two tables: `danfs_ships` with all ships in the [DANFS](http://www.history.navy.mil/research/histories/ship-histories/danfs.html), and `confederate_ships` with all ships in the [Confederate ships](http://www.history.navy.mil/research/histories/ship-histories/confederate_ships.html) histories.
+
--- danfs.py
+++ danfs.py
@@ -145,18 +145,18 @@ def insert_danfs(con):
                     'history': client.get_ship_text(ship['path']),
                     'url': client.get_ship_url(ship['path'])
                     }
-        con.execute(ins, **data)
+            con.execute(ins, **data)
 
        
 def main():
     DB = "sqlite:///dfas.sqlite3"
     engine = create_engine(DB)
     metadata.bind = engine
-    metadata.drop_all()
-    metadata.create_all()
+    #metadata.drop_all()
+    metadata.create_all(checkfirst = True)
     con = metadata.bind
     insert_danfs(con)
-    insert_confederate_ships(con)
+    #insert_confederate_ships(con)
     
 if __name__ == "__main__": 
     main()
